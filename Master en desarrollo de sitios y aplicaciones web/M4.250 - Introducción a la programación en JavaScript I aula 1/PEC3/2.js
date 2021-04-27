4af3c0ffeaf2c16e161c4c531348536c2de79d63
schartun <schartun@gmail.com>
2021-04-27 11:53:16 -0500

    Update 2.js PEC3

--- Master en desarrollo de sitios y aplicaciones web/M4.250 - Introducción a la programación en JavaScript I aula 1/PEC3/2.js
+++ Master en desarrollo de sitios y aplicaciones web/M4.250 - Introducción a la programación en JavaScript I aula 1/PEC3/2.js
@@ -22,3 +22,67 @@ console.log(yesterday.toLocaleDateString());
 // Obteber el año 
 let año  = yesterday.getFullYear();
 console.log(año);
+
+
+
+//Otra solucion mas robusta
+
+
+
+let string = '1/2/2019';
+let info = "";
+let c = 0;
+let j = 0;
+let array = [];
+
+for (i = 0; i <= string.length; i++ ) {
+  if ( string[i] === '/' || string[i] === '-' ) {
+    info = string.slice(c, i);
+    array[j] = info;
+    c = c + (info.length + 1);
+    j++
+  } 
+  info = string.slice(c, i);
+  array[j] = info
+}
+
+let DD = array[0];
+let MM = array[1] -1;
+let YYYY = array[2];
+
+let fecha = new Date(YYYY, MM, DD);
+let msPerDay = 24 * 60 * 60 * 1000;  // Número de milisegundos por día
+msYesterday =  fecha - msPerDay;
+yesterday = new Date(msYesterday);
+console.log(yesterday.toLocaleDateString());
+
+
+//tercera solucion pero esta es la que usare
+
+
+let date = prompt("Introduzca fecha en formato 'DD/MM/AAAA':");
+
+//Comprobar que siga el formato correcto el string
+
+if (date.includes('/') === true && date.slice(0,3).includes('/')) {
+
+  //convert string to date
+  
+  var dattmp = date.split('/').reverse().join('/');
+  var nwdate =  new Date(dattmp);
+
+  
+  // Número de milisegundos por día
+  let msPerDay = 24 * 60 * 60 * 1000; 
+
+  // Resta de dias en ms
+  let msYesterday =  nwdate - msPerDay;
+
+  let yesterday = new Date(msYesterday);
+
+  console.log("El dia de ayer fue: " + yesterday.toLocaleDateString(('en-GB')));  
+} else {
+  console.log("Formato de fecha no valida, cargue nuevamente el programa e intenete con formato correcto");
+}
+  
+
