757e74db344bd3f4ac885bf59e7fd5c7e2702795
schartun <schartun@gmail.com>
2021-05-05 16:56:15 -0500

    Update 3.js PEC3 JS

--- Master en desarrollo de sitios y aplicaciones web/M4.250 - Introducción a la programación en JavaScript I aula 1/PEC2/3.js
+++ Master en desarrollo de sitios y aplicaciones web/M4.250 - Introducción a la programación en JavaScript I aula 1/PEC2/3.js
@@ -4,31 +4,33 @@ PEC 2
 Alumno: Ivan Schartun
 */
 
+
+
 // Declaracion de variables principales a ser utilizadas
 
-
-let numero = prompt("Inserte numero entero real: ");
+let numero = prompt("Introduzca un numero entero positivo: ");
 let resultado = 0;
 let serieSuma = "";
 
 
-// Metodo de calculo 
+//Comprobacion que los valores ingresados son numeros entereos positivos, y no sean float (decïmal con "."), ni strings
 
-if ( numero === "" || numero === undefined || isNaN(numero) === true || numero % 1 !== 0 || numero < 0 ) {
+if ( isNaN(numero) || numero % 1 !== 0 || numero <= 0 ) {
   console.log("Valor insertado no es valido")
 } else {
 
-  // Cambio de string a numero entero 
+ // Método de calculo
+  
   numero = parseInt(numero);
   
   for ( i = 1; i < (numero + 1); i++ ) {
     serieSuma = serieSuma + i + " + " ;
-    
-    //removemos el ultimo " + " generado
-    
     secuencia = serieSuma.slice(0, -3);  
     resultado += i;
   }
+ 
+ // Método de presentación por consola
+  
   console.log(secuencia + " = " + resultado);
 }
 
