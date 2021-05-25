3dd310a7b0e7ebd04883b7842f9d0927a6403b50
schartun <schartun@gmail.com>
2021-05-24 22:04:19 -0500

    Update PEC3 [mars-2020 css]

--- Master en desarrollo de sitios y aplicaciones web/M4.252 - HTML y CSS aula 1/PEC3/Ejercicio/css/estilos_raw.cs
+++ Master en desarrollo de sitios y aplicaciones web/M4.252 - HTML y CSS aula 1/PEC3/Ejercicio/css/estilos_raw.cs
@@ -1,9 +1,10 @@
+/* font-family: 'Alegreya', serif;
+font-family: 'Open Sans', sans-serif; */
 /*
 -------------------------------------------------------------------------------------------------------------------
 En esta sección se dará formato general a los elementos generales del sitio web, comunes para todas las paginas
 -------------------------------------------------------------------------------------------------------------------
 */
-
 /* Regla que permite establecer variables generales de colores */
 :root {
   --text-color: rgb(0, 0, 0);
@@ -77,9 +78,7 @@ h3 {
 En esta sección se establecerá un contenedor general para todas las páginas. Al mismo tiempo se definirán los cambios que deben ejecutarse según dimensión del viewport
 -------------------------------------------------------------------------------------------------------------------
 */
-
-/*Regla que estable la dimensión general  del contendor general para todas las paginas */
-
+/* en esta regla se estable la dimensión general  del contendor general para todas las paginas */
 .container {
   margin-right: auto;
   margin-left: auto;
@@ -92,11 +91,29 @@ En esta sección se establecerá un contenedor general para todas las páginas. 
   }
 }
 
+/* @media (min-width: 768px) {
+  .container {
+    max-width: 720px;
+  }
+} */
+
 @media (min-width: 950px) {
   .container {
     max-width: 800px;
   }
 }
+
+/* @media (min-width: 992px) {
+  .container {
+    max-width: 960px;
+  }
+} */
+
+/* @media (min-width: 1200px) {
+  .container {
+    max-width: 1000px;
+  }
+} */
 
 main {
   padding-top: 3rem;
@@ -108,14 +125,12 @@ main {
 En esta sección se dará formato al header del sitio web que es común para todas sus paginas
 -------------------------------------------------------------------------------------------------------------------
 */
-
 /* Regla dar espacio de barra de navegacion y tope de cabecera*/
 div.container {
   margin-top: 0.9rem;
 }
 
 /* Regla para horizontalizar los enlaces del menu */
-
 nav {
   margin-top: 0.6rem;
 }
@@ -135,7 +150,6 @@ div.container img {
 }
 
 /* Reglas para dar formato a cada enlace en el menú */
-
 nav a {
   color: var(--text-color);
   font-family: "Open Sans", sans-serif;
@@ -151,7 +165,6 @@ nav li.active {
 }
 
 /* Reglas para modificar propiedades a pantallas mayores a 768px */
-
 @media (min-width: 950px) {
   nav ul {
     flex-direction: row;
@@ -169,14 +182,57 @@ nav li.active {
   }
 }
 
+/* Regla dar espacio de barra de navegacion y tope de cabecera*/
+/* div.container {
+  margin-top: 0.9rem;
+} */
+/* Regla para horizontalizar los enlaces del menu */
+/* nav {
+  margin-top: 0.6rem;
+}
+
+nav ul li {
+  font-size: 1.1rem;
+  margin-right: 1.2rem;
+  text-align: center;
+} */
+/* Reglas para dar formato a cada enlace en el menú */
+/* nav a {
+  color: var(--text-color);
+  font-family: "Open Sans", sans-serif;
+  font-weight: 800;
+}
+
+nav a:hover {
+  color: var(--contrast-color);
+}
+
+nav li.active {
+  border-bottom: 3px solid var(--text-color);
+}
+
+div.container a img {
+  margin: 0 auto;
+  display: block;
+} */
+/* Query para permitir que un view port mas amplio se coloquen de forma horizontal los items den nav */
+/* @media (min-width: 600px) {
+  nav ul li {
+    display: inline-block;
+  }
+
+  div.container a img {
+    display: inherit;
+    width: 20%;
+    height: auto;
+  }
+} */
 /*
 -------------------------------------------------------------------------------------------------------------------
 En esta sección se dará formato al footer del sitio web que es común para todas sus paginas
 -------------------------------------------------------------------------------------------------------------------
 */
-
 /* Regla para dar estilo general a la seccion de footer */
-
 footer {
   padding-top: 3rem;
   padding-bottom: 1.5rem;
@@ -187,7 +243,6 @@ footer {
 }
 
 /* Regla para adecuar el tamano del logo */
-
 footer div.container img {
   display: block;
   margin: 0 auto;
@@ -196,15 +251,13 @@ footer div.container img {
 }
 
 /* Regla para adecuar correo electornico en footer */
-
-footer div.container > a {
+footer div.container>a {
   display: block;
   font-size: 0.8rem;
   margin-top: 0.6rem;
 }
 
 /* Reglas para dar formato a cada enlace en el footer  */
-
 footer a {
   color: var(--text-bg-color);
 }
@@ -214,13 +267,11 @@ footer a:hover {
 }
 
 /* Reglas para dar estilo a newsletter */
-
 footer span {
   font-style: italic;
 }
 
 /* Reglas para dar estilo a los campos de la forma */
-
 footer form {
   margin-top: 3rem;
   margin-bottom: 2rem;
@@ -264,7 +315,6 @@ button:hover {
 }
 
 /* Reglas para dar estilo al menu de navegación en footer */
-
 footer ul li {
   display: inline-block;
   font-size: 0.7rem;
@@ -272,7 +322,7 @@ footer ul li {
   margin-right: 0.2rem;
 }
 
-div.bottomFooter ul + p {
+div.bottomFooter ul+p {
   margin-top: 1.8rem;
 }
 
@@ -300,7 +350,7 @@ div.bottomFooter p {
     font-size: 1.1rem;
   }
 
-  form > p {
+  form>p {
     font-size: 1.5rem;
   }
 
@@ -309,11 +359,116 @@ div.bottomFooter p {
   }
 }
 
+/* Regla para dar estilo a la seccion de footer, color, espacio */
+/* footer {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  background-color: var(--bg-dark-color);
+  color: var(--text-bg-color);
+  font-family: "Open Sans", sans-serif;
+  text-align: center;
+} */
+/* footer div.container {
+  display: grid;
+  justify-items: center;
+  font-size: 0.8rem;
+} */
+/* footer ul li a {
+  font-size: calc(0.6rem + 0.3vw);
+}
+
+footer ul li {
+  margin-left: auto;
+  margin-right: auto;
+} */
+/* Reglas para dar formato a cada enlace en el footer  */
+/*
+footer a {
+  color: var(--text-bg-color);
+}
+
+footer a:hover {
+  color: var(--contrast-color);
+} */
+/* Regla dar espacio en el límite superior de la forma*/
+/* form {
+  margin-top: 3rem;
+} */
+/* Reglas para estilo  de texto newsletter */
+/* form > p {
+  font-size: calc(0.7rem + 1.5vw);
+}
+
+form span {
+  font-style: italic;
+} */
+/* Reglas para dar estilo a los campos de la forma */
+/* ::placeholder {
+  color: grey;
+  text-align: center;
+  font-size: 1.5rem;
+}
+
+input[type="email"] {
+  width: 100%;
+  padding: 1.8rem;
+  margin: 0.8rem 0;
+  border: none;
+  background-color: var(--bg-dark-color);
+  border-bottom: 0.1rem solid var(--text-bg-color);
+}
+
+footer label {
+  font-size: calc(0.7rem + 0.3vw);
+}
+
+button {
+  border: none;
+  padding: 0.5rem 0.8rem;
+  font-size: 1.4rem;
+  margin: 0.4rem 0.2rem;
+}
+
+button:hover {
+  background-color: var(--contrast-color);
+  color: var(--text-bg-color);
+}
+
+.bottomFooter ul li {
+  display: inline;
+  margin-right: 1.2rem;
+  font-size: 1.1rem;
+}
+
+div.bottomFooter p {
+  margin-bottom: 0.5rem;
+  font-size: 0.8rem;
+}
+
+div.bottomFooter ul + p {
+  margin-top: 1.8rem;
+}
+
+.bottomFooter {
+  margin-bottom: 2rem;
+}
+
+footer div.container img {
+  width: 60%;
+  height: auto;
+}
+
+@media (min-width: 600px) {
+  footer div.container img {
+    width: 25%;
+  }
+} */
 /*
 -------------------------------------------------------------------------------------------------------------------
 En esta sección se dará formato a index.htnl
 -------------------------------------------------------------------------------------------------------------------
 */
+
 
 /* Cabecera de sección de articulo, fondo e imagen */
 
@@ -335,17 +490,17 @@ section.hero div {
 }
 
 section.hero h1 {
-  margin-top: 1.5rem;
+  margin-top:1.5rem;
   font-size: 1.8rem;
 }
 
-section.hero p {
+section.hero  p {
   font-style: italic;
   margin-top: 0.6rem;
 }
 
-ul.grid li img {
-  display: block;
+ul.grid li img  {
+  display:block;
   max-width: 100%;
 }
 
@@ -354,75 +509,132 @@ ul.grid li img {
 @media (min-width: 480px) {
   section.hero {
     background-size: 20rem auto;
-    background-position: 90% bottom;
+    background-position: 90%  bottom;
     height: 20rem;
   }
-}
 
 @media (min-width: 950px) {
+  /* section.hero {
+    background-size: 20rem auto;
+    background-position: 90%  bottom;
+    height: 20rem;
+  } */
+
   section.hero div {
     padding-top: 0.2rem;
   }
 
   section.hero h1 {
+  font-size: 4rem;
+
+}
+
+  section.hero  p {
+    font-size: 1.9rem;
+
+}
+
+/* Cabecera de sección de articulo */
+/* section.hero {
+  background: url(../img/bg-hero-mars.jpg);
+  background-repeat: no-repeat;
+  background-size: 70% auto;
+  background-position: right bottom;
+  background-color: black;
+  color: var(--text-bg-color);
+  height: 20rem;
+}
+
+section.hero div {
+  padding-top: 2rem;
+  padding-left: 0.8rem;
+}
+
+section.hero div h1 {
+  font-size: calc(1.7rem + 1.6vw);
+}
+
+section.hero div p {
+  font-style: italic;
+  font-size: calc(0.9rem + 1.5vw);
+} */
+
+/* Media query para pantallas superiores a px */
+/* @media (min-width: 600px) {
+  section.hero div {
+    margin-left: 10rem;
+  }
+
+  section.hero div h1 {
     font-size: 4rem;
   }
 
-  section.hero p {
-    font-size: 1.9rem;
+  section.hero {
+    background-size: contain;
+    background-position: right;
+    height: 25rem;
   }
+} */
 
-  /* Reglas para encabezados de sección foto misiones */
-  section.container h2 {
-    margin-top: 2rem;
-    font-size: 1.7rem;
-  }
-
-  section.container p {
-    font-size: 1.2rem;
-  }
-
-  ul.grid h3 {
-    font-size: 1.5rem;
-  }
-
-  ul.grid p {
-    font-size: 1rem;
-  }
-
-  ul.grid li h3 {
-    padding-left: 0.8rem;
-  }
-
-  ul.grid li p {
-    padding-left: 0.8rem;
-    padding-right: 0.9rem;
-  }
+/* Reglas para encabezado de sección foto misiones */
+section.container h2 {
+  margin-top: 2rem;
+  font-size: 1.7rem;
 }
+
+section.container p {
+  font-size: 1.2rem;
+}
+
+ul.grid h3 {
+  font-size: 1.5rem;
+}
+
+ul.grid p {
+  font-size: 1rem;
+  
+}
+
+
+ul.grid li h3 {
+  padding-left:0.8rem;
+}
+
+ul.grid li p {
+  padding-left:0.8rem;
+  padding-right: 0.9rem;
+}
+}
+
 
 @media (min-width: 576px) {
   ul.grid {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-  ul.grid h3 {
-    font-size: 1.3rem;
-  }
+    grid-template-columns:repeat(3, 1fr);
 
-  ul.grid p {
-    font-size: 0.8rem;
   }
+ul.grid h3 {
+  font-size: 1.3rem;
+}
+
+ul.grid p {
+  font-size: 0.8rem;
+}
 
   ul.grid li:nth-child(odd) {
-    background-color: var(--bg-light-color);
-  }
+  background-color: var(--bg-light-color);
 }
+
+}
+
+
 
 /*
 -------------------------------------------------------------------------------------------------------------------
 En esta sección se dará formato a mars-2020.html
 -------------------------------------------------------------------------------------------------------------------
 */
+
 
 /* Regla que permite dar formato a la lista interna del artículo */
 
@@ -459,56 +671,56 @@ iframe.video-fluid-item {
   content: "";
   padding-top: 56.25%;
 }
-
-iframe {
-  border: 0;
-}
-
 /* 
 Regla para dar formato a la leyenda de video */
 
 article div p {
-  font-size: 0.6rem;
-  text-align: center;
-  margin-top: 0.6rem;
+font-size: 0.6rem;
+text-align:center;
+margin-top:0.6rem;
+
 }
 /* Reglas para dar formato a menú de navegación secundario */
 
 aside {
-  font-family: "Open Sans", sans-serif;
+
+font-family: 'Open Sans', sans-serif;
 }
 
 aside h3 {
-  font-size: 0.9rem;
-  font-weight: 400;
+font-size: 0.9rem;
+font-weight:400
 }
 
 aside ul {
-  display: flex;
-  flex-direction: column;
-  list-style-type: none;
-  font-size: 0.8rem;
+display: flex;
+flex-direction: column;
+list-style-type: none;
+font-size:0.8rem
 }
 
 .activeAside {
-  font-weight: 800;
+font-weight:800;
 }
 
 /* Reglas Query para pantallas superiores a 950px */
 
 @media (min-width: 950px) {
-  article {
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-  }
 
-  article section {
-    grid-column: 2/3;
-  }
-
-  article aside {
-    grid-column: 1 / 2;
-    grid-row: 1/2;
-    margin-top: 3rem;
-  }
+article {
+  display:grid;
+  grid-template-columns: 1fr 2fr;
 }
+
+article section {
+grid-column: 2/3 ;
+}
+
+article aside {
+  grid-column: 1 / 2 ;
+  grid-row: 1/2;
+  margin-top:3rem;
+}
+
+}
+
